3bd2234a065c8380cd3315547a644daaf5145321
Andrei <borodin_a_o@sc.vsu.ru>
2021-03-30 11:52:38 +0300

    cleanup

--- src/main/java/org/equilibrium/configuration/ConfigurationStorage.java
+++ src/main/java/org/equilibrium/configuration/ConfigurationStorage.java
@@ -1,18 +1,12 @@
 package org.equilibrium.configuration;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.rethinkdb.RethinkDB;
 import com.rethinkdb.net.Connection;
-import com.rethinkdb.net.Cursor;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.enterprise.context.ApplicationScoped;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
 
 @ApplicationScoped
@@ -20,8 +14,6 @@ public class ConfigurationStorage {
 
     private static final Logger logger = LoggerFactory.getLogger(ConfigurationStorage.class);
     public static final RethinkDB r = RethinkDB.r;
-
-    private Routes routes;
 
     @ConfigProperty(name = "configuration-storage.hostname")
     String hostname;
